593fd3a2ad2f27a75f38fb97796e55094b28b162
MikeMaesod <maesodinfo@gmail.com>
2021-07-10 01:52:13 +0700

    Recover P'Maew OK
    Recover P'Maew OK1

--- js/bot.js
+++ js/bot.js
@@ -345,9 +345,13 @@ class bot {
               urlServerMine = `https://mine.tlmminer.com?wallet=${wax.userAccount}&hashfail=${hashfail}&last_mine_tx=${last_mine_tx}&difficulty=${difficulty}`
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
               nonce = await this.postData(urlServerMine, {}, 'GET', { Origin: "" }, 'raw') 
             }
 
+=======
+            }
+>>>>>>> parent of 76f5d24 (P'Maew OK Edit TLM New)
 =======
             }
 >>>>>>> parent of 76f5d24 (P'Maew OK Edit TLM New)
